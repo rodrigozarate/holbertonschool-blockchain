4edee6350a28cc40d1392d890731640f65757071
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-12-04 12:39:55 -0500

    [FIX] add missing structure

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -17,6 +17,17 @@
 #define PUB_FILENAME "key_pub.pem"
 #define PRI_FILENAME "key.pem"
 
+/**
+ * struct sig_s - store signature of EC_KEY key pair
+ * @sig: byte array
+ * @len: size
+ */
+typedef struct sig_s
+{
+	uint8_t sig[SIG_MAX_LEN];
+	uint8_t len;
+} sig_t;
+
 
 uint8_t *sha256(int8_t const *s, size_t len,
 		uint8_t digest[SHA256_DIGEST_LENGTH]);
